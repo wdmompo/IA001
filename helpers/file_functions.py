5e00f3b1dd753d47f8a1cbc0db326020070f0ccc
Walter Damian Mompo <wdmompo@gmail.com>
2026-09-02 19:13:00 -0300

    Ajuste de archivos de input

--- helpers/file_functions.py
+++ helpers/file_functions.py
@@ -10,9 +10,10 @@ logger = get_logger(__name__)
 @medir_tiempo_ms
 def cargar_datos_desde_txt(nombre_archivo: str) -> list:
     try:
-        with open(path.join(path.dirname(path.dirname(__file__)), nombre_archivo), 'r', encoding='utf-8') as f:
+        print(f"Loading data from file: {path.join(path.join(path.dirname(path.dirname(__file__)), 'inputs'), nombre_archivo)}")
+        with open(path.join(path.join(path.dirname(path.dirname(__file__)), "inputs"), nombre_archivo), 'r', encoding='utf-8') as f:
             return [line.strip() for line in f.readlines() if line.strip()]
     except FileNotFoundError:
-        logger.error(f"File not found: {path.join(path.dirname(path.dirname(__file__)), nombre_archivo)}")
-        print(f"File not found: {path.join(path.dirname(path.dirname(__file__)), nombre_archivo)}")
+        logger.error(f"File not found: {path.join(path.join(path.dirname(path.dirname(__file__)), 'inputs'), nombre_archivo)}")
+        print(f"File not found: {path.join(path.join(path.dirname(path.dirname(__file__)), 'inputs'), nombre_archivo)}")
         return []
